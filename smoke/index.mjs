4783f662b0463135926a4288b3fc062abfcbcac4
Lyra Naeseth <lyra@lyra.codes>
2022-09-23 01:57:45 -0700

    Add monotonic UUID generation

--- smoke/index.mjs
+++ smoke/index.mjs
@@ -1,7 +1,11 @@
 /* eslint-env node */
 
-import uuid, { timestamp } from 'uuid7';
+import uuid, { generator, timestamp } from 'uuid7';
 
 const id = uuid();
 console.log(id);
 console.log(new Date(timestamp(id)));
+console.log();
+
+const gen = generator();
+for (let i = 0; i < 8; i++) console.log(gen());
