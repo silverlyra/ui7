118b47b486911af8fe5fa40f832b67716c775939
Lyra Naeseth <lyra@lyra.codes>
2022-09-23 19:46:53 -0700

    Use an available npm package name (#3)
    `uuid7` is disallowed by npm as being too similar to `uuid`.

--- smoke/index.cjs
+++ smoke/index.cjs
@@ -1,7 +1,7 @@
 /* eslint-env node */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const { v7, generator, timestamp } = require('uuid7');
+const { v7, generator, timestamp } = require('ui7');
 
 const id = v7();
 console.log(id);
